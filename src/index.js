f11cef09da582549fff87835208bfd731f179191
hongfei <hongfei.tian@mobilemd.cn>
2020-07-13 19:01:55 +0800

    dva remove

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import ReactDom from 'react-dom';
 import { ConfigProvider } from 'antd';
 import App from "./App";
 import zhCN from 'antd/es/locale/zh_CN';
-import dva from 'dva';
 import 'antd/dist/antd.css';
 import './index.less';
-// const app = dva();
-// app.model(require('./models/try').default);
+
 class Index extends React.Component {
     render () {
         console.log('全局环境的参数 <CONFIG>', CONFIG);
         return (
             <ConfigProvider locale={zhCN}>
                 <App />
-                {/* { app.router(() => <App />) } */}
             </ConfigProvider>
         )
     }
 }
-// app.start('#app');
 ReactDom.render(<Index />, document.getElementById("app"))
